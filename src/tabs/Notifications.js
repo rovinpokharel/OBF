48b09a846c9a4fb7782332529cd41fb00130cc33
Rovin Pokharel <rovin@dhisolutions.com>
2024-05-06 18:31:06 +0545

    added dashboard for admin and user

---
+++ src/tabs/Notifications.js
@@ -0,0 +1,25 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Notifications = () => {
+  return (
+    <View>
+      <Text>Notifications</Text>
+    </View>
+  )
+}
+
+export default Notifications
+
+// import {View, Text} from 'react-native';
+// import React from 'react';
+
+// const Notifications = () => {
+//   return (
+//     <View>
+//       <Text>Notifications</Text>
+//     </View>
+//   );
+// };
+
+// export default Notifications;
