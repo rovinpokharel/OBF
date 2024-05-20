fadff6dfebf841d3fa62bc7c166c5b893190b7f4
Rovin Pokharel <rovin@dhisolutions.com>
2024-05-20 08:47:20 +0545

    Added navigation path and Transactions code

--- src/tabs/Transactions.js
+++ src/tabs/Transactions.js
@@ -10,16 +10,3 @@ const Transactions = () => {
 }
 
 export default Transactions
-
-// import { View, Text } from 'react-native'
-// import React from 'react'
-
-// const Transactions = () => {
-//   return (
-//     <View>
-//       <Text>Transactions</Text>
-//     </View>
-//   )
-// }
-
-// export default Transactions
