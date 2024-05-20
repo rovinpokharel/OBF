2f54948f47e4a6ce58215a1c7a34b1541e6c351d
Rovin Pokharel <rovin@dhisolutions.com>
2024-05-20 08:50:31 +0545

    Added notifications and view orders code

--- src/tabs/Notifications.js
+++ src/tabs/Notifications.js
@@ -10,16 +10,3 @@ const Notifications = () => {
 }
 
 export default Notifications
-
-// import {View, Text} from 'react-native';
-// import React from 'react';
-
-// const Notifications = () => {
-//   return (
-//     <View>
-//       <Text>Notifications</Text>
-//     </View>
-//   );
-// };
-
-// export default Notifications;
